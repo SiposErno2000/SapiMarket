7afde4603f953496d1b0c8c600cae502d7a01ed9
Nagy Krisztian Endre <krisztian.nagy@3ss.tv>
2021-05-14 13:16:57 +0300

    Add Item fragment_add_item.xml

--- app/src/main/java/com/example/sapimarket/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/sapimarket/ui/home/HomeFragment.java
@@ -3,14 +3,19 @@ package com.example.sapimarket.ui.home;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.sapimarket.R;
+import com.example.sapimarket.ui.addItem.AddItemFragment;
 
 public class HomeFragment extends Fragment {
+
+    private Button addButton;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -20,6 +25,18 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        addButton = view.findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.replace(R.id.fragment_container,new AddItemFragment());
+                fragmentTransaction.commit();
+            }
+        });
+        return view;
     }
 }
